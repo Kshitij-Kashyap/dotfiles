5d64e88e793f3f48299d96df8d2a184506da6e5f
Kshitij Kashyap <97724306+Kshitij-Kashyap@users.noreply.github.com>
2024-09-19 23:44:09 +0530

    Update vim-options.lua

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -3,6 +3,11 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set numberwidth=2")
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.signcolumn = "number"
+
 vim.g.mapleader = " "
 vim.keymap.set('n', '<leader>n', ':set rnu<CR>')
-
+vim.keymap.set('n', '<C-s>', ':w<CR>')
